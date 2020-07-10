432a28a9c2606a2ebe803847838c1f7bf7c81026
sbalasubramani0505 <sbalasubramani0505@users.noreply.github.com>
2020-07-10 10:32:20 -0700

    Add files via upload

--- mapper.py
+++ mapper.py
@@ -1,51 +1,19 @@
 #!/usr/bin/env python
-#Imports
+
 import sys
-import os
-import re
-import datetime
 
-#File operation
-file = sys.stdin
-file.next()
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        print '%s\t%s' % (word, 1)
 
-#Iterate through file
-for line in file:
-    elements = line.strip().split("\t")
-    if elements.__len__() == 7 :
-        Error_Msg = ""
-        if ":" in elements[0]:  ##Split first field into Orderid and Date
-            Order_id_dt = re.sub('\:+', ':',elements[0]).split(":")
-            Order_id = Order_id_dt[0]
-            ## Reformat the date if valid
-            if Order_id_dt[1] and datetime.datetime.strptime(Order_id_dt[1], '%Y%m%d').strftime('%Y-%m-%d'):
-                Order_dt = datetime.datetime.strptime(Order_id_dt[1], '%Y%m%d').strftime('%Y-%m-%d')
-            else:
-                Order_dt = Order_id_dt[1]
-                if (Error_Msg.__len__() != 0):
-                    Error_Msg = Error_Msg + "| Invalid Date"
-                else:
-                    Error_Msg = Error_Msg + "Invalid URL"
-        else:
-            Order_id = elements[0]
-            Order_dt = "null"
-        URL = ""
-        if (not elements[6].startswith('http://www.insacart.com/')):
-            if ( Error_Msg.__len__() != 0):
-                Error_Msg = Error_Msg + "| Invalid URL"
-            else:
-                Error_Msg = Error_Msg + "Invalid URL"
-        else:
-            URL = elements[6] ## Valid URL
-        Sum_Item = 0
-        Item_Cnt = 0
-	## Find average of items
-        for i in range(2, 6):
-            Sum_Item = Sum_Item + float((elements[i] or 0))
-            if (elements[i] and elements[i] != "0"):
-                Item_Cnt = Item_Cnt + 1
-	if (Item_Cnt != 0):
-        	Avg_Item = Sum_Item / Item_Cnt
-        print Order_id + "\t" + Order_dt + "\t" + elements[1] + "\t" + str(Avg_Item) + "\t" + URL + "\t" + Error_Msg
-    else:
-        print line.rstrip('\n') + "\t" + "Malformed Record" ## If number of fields not equal to 7 -- Add malformed record msg
